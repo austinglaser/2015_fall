030c4f7f14b0ded61245f7b93baefbbbeb8e8dcc
Austin Glaser <austin@boulderes.com>
2015-09-04 11:31:01 -0600

    set up huen-indy directory

---
+++ huen-indy/notes/todays_notes.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+date_str="$(date +%Y-%m-%d)"
+filename="md/$date_str.md"
+header="$date_str - TOPIC"
+header_len=${#header}
+
+if [ ! -f $filename ] ; then
+    echo $header > $filename
+    for i in $(seq 1 $header_len) ; do
+        echo -n "=" >> $filename
+    done
+    echo "" >> $filename
+
+    echo "" >> $filename
+
+    for i in $(seq 1 80) ; do
+        echo -n "-" >> $filename
+    done
+    echo "" >> $filename
+
+    echo "" >> $filename
+    echo "# Assignments" >> $filename
+    echo "" >> $filename
+
+    for i in $(seq 1 80) ; do
+        echo -n "-" >> $filename
+    done
+    echo "" >> $filename
+
+    echo "" >> $filename
+fi
+
+vim $filename
